3365f163a166a6925dfa45349c5189b2051b9283
Akash Mahajan <akashmahajan025@gmail.com>
2022-09-26 21:48:53 +0530

    Dataset updated

--- balance_dataset.py
+++ balance_dataset.py
@@ -4,5 +4,12 @@ This code balances dataset using newly found transactions that are fraud but lab
 import pandas as pd
 
 df = pd.read_csv('data/transactions.csv')
-# fraud_rule = ((df.oldbalanceOrg - df.newbalanceOrig) - (df.newbalanceDest - df.oldbalanceDest)) == 0
-print(df.head())
+
+df['origin_fraud'] = ((df.oldbalanceOrg - df.newbalanceOrig) - (df.newbalanceDest - df.oldbalanceDest)) == 0
+fraud = df[df['origin_fraud'] == True].index
+
+for i in fraud:
+    df.at[i, 'isFraud'] = 1
+
+df.drop(columns=['origin_fraud'], inplace=True)
+df.to_csv('data/transactions.csv', index=False)
